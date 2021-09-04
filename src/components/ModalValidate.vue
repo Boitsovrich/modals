a7f958d70f6e67a1cce22c1e0619f8e08b723ecf
aboytsov <aboytsov@phoenixit.ru>
2021-09-04 13:24:17 +0300

    master

--- src/components/ModalValidate.vue
+++ src/components/ModalValidate.vue
@@ -1,30 +1,32 @@
 <template>
-  <modal
-    title="Modal with form + Validate"
-    @close="$emit('close')">
+  <modal title="Modal with form + Validate" @close="$emit('close')">
     <!-- body -->
     <div slot="body">
       <form @submit.prevent="onSubmit">
         <!-- name -->
         <div class="form-item" :class="{ errorInput: $v.name.$error }">
           <label>Name:</label>
-          <p class="errorText" v-if="!$v.name.required"> Filed is required! </p>
-          <p class="errorText" v-if="!$v.name.minLength"> Name must have at least {{$v.name.$params.minLength.min}} !</p>
+          <p class="errorText" v-if="!$v.name.required">Filed is required!</p>
+          <p class="errorText" v-if="!$v.name.minLength">
+            Name must have at least {{ $v.name.$params.minLength.min }} !
+          </p>
 
           <input
-          v-model="name"
-          :class="{ error: $v.name.$error }"
-          @change="$v.name.$touch()">
+            v-model="name"
+            :class="{ error: $v.name.$error }"
+            @change="$v.name.$touch()"
+          />
         </div>
         <!-- email -->
         <div class="form-item" :class="{ errorInput: $v.email.$error }">
           <label>Email:</label>
-          <p class="errorText" v-if="!$v.email.required"> Filed is required! </p>
-          <p class="errorText" v-if="!$v.email.email"> Email is not correct! </p>
+          <p class="errorText" v-if="!$v.email.required">Filed is required!</p>
+          <p class="errorText" v-if="!$v.email.email">Email is not correct!</p>
           <input
-          v-model="email"
-          :class="{ error: $v.email.$error }"
-          @change="$v.email.$touch()">
+            v-model="email"
+            :class="{ error: $v.email.$error }"
+            @change="$v.email.$touch()"
+          />
         </div>
         <!-- button -->
         <button class="btn btnPrimary">Submit!</button>
@@ -58,34 +60,38 @@ export default {
       email,
     },
   },
-  methods:{
-    onSubmit () {
-      this.$v.$touch()
-      if(!this.$v.$invalid) {
+  methods: {
+    onSubmit() {
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
         const user = {
           name: this.name,
-          email: this.email
-        }
-        console.log(user)
+          email: this.email,
+        };
+        console.log(user);
+        // Done!
+        this.name = "";
+        this.email = "";
+        this.$v.$reset();
+        this.$emit("close");
       }
-
-    }
-  }
+    },
+  },
 };
 </script>
 <style lang="scss">
-.form-item .errorText{
+.form-item .errorText {
   display: none;
   margin-bottom: 8px;
   font-size: 13.4px;
   color: #de4040;
 }
 .form-item {
-  &.errorInput .errorText{
-  display: block;
+  &.errorInput .errorText {
+    display: block;
   }
 }
-input.error{
+input.error {
   border-color: #de4040;
 }
 </style>
